258a3ac250c3ce5b8f122fe21ed69ad0772e25d5
Billy Beck <billys.beck@gmail.com>
2023-08-20 10:09:47 -0400

    New Page Transitions
    New Pages Transitions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+import EnglishPeaks from './Components/englishPeaks';
+import FrenchPeaks from './Components/frenchPeaks';
+import FinnishPeaks from './Components/finnishPeaks';
 import './App.css';
+import './fonts/Tangerine/Tangerine-Regular.ttf';
+
+
+
+
+const items = [ <EnglishPeaks/>, <FrenchPeaks/>, <FinnishPeaks/> ]
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [mediaItem, setMediaItem] = React.useState(items[0]); // <-- seed initial state
+  const [index, setIndex] = React.useState(0);
+
+      
+  React.useEffect(() => {
+    const timerId = setInterval(
+        () => setIndex((i) => (i + 1) % items.length), // <-- increment index
+        13000
+      );
+      return () => clearInterval(timerId);
+    }, []);
+  
+    React.useEffect(() => {
+      setMediaItem(items[index]); // <-- update media state when index updates
+    }, [index]);
+  
+    return (
+      <div className="App">
+      <header className='App-header'>
+        <div className='font-face-tangerine'>
+        
+        <div className='poet'>
+        <span>{mediaItem}</span>
+        </div>
+        
+        
+        </div>
+        </header>
+     
+      <footer>
+      </footer>
+      </div>
+    );
+
 }
 
 export default App;
